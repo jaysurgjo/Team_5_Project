131efa2e635f7af8d52dd3098032203014552abb
Ebrima Saho <saho.ebrima@ymail.com>
2022-05-16 10:40:32 -0700

    Added methods in controller

--- src/com/etrack/controller/EtrackController.java
+++ src/com/etrack/controller/EtrackController.java
@@ -1,6 +1,45 @@
 package com.etrack.controller;
 
+import java.util.Scanner;
+
 class EtrackController {
 
+    private Scanner scanner = new Scanner(System.in);    // String - nextLine  Integer- nextInt
+
+
+
     //created model class
+    public void execute(){
+
+    }
+
+    // show products available
+
+
+    // Select product
+    public void selectProduct(){
+
+    }
+
+
+    // Update cart
+    public void updateCart() {
+
+    }
+
+    public void displayCart() {
+
+    }
+
+    //
+    public String promptForName() {
+
+    }
+
+
+    private void welcome() {
+        System.out.println("\n");
+        System.out.println("E-TRACK");
+        System.out.println("\n");
+    }
 }

f3ef27bcd68245a33b5d42be8c4db4d1b5921416
Ebrima Saho <saho.ebrima@ymail.com>
2022-05-17 14:47:25 -0700

    logic flow

--- src/com/etrack/controller/EtrackController.java
+++ src/com/etrack/controller/EtrackController.java
@@ -1,56 +1,101 @@
 package com.etrack.controller;
 
-
+import com.etrack.Shopper;
+import com.etrack.Product;
 import com.etrack.view.EtrackView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-class EtrackController {
+public class EtrackController {
     private Scanner scanner = new Scanner(System.in);    // String - nextLine  Integer- nextInt
+    private int productId;
+
     //created model class
 
     public void execute(){
         welcome();
+        promptForCustomerName();
+        showProductList();
+        promptForProductId();
+        addToCart();
+        promptToContinueShopping();
         displayCart();
+        checkOut();
      }
- 
-
-    // show products available
-
-
-    // Select product
-    public void selectProduct() {
-    }
-
-    // Update cart
-    public void updateCart(int id, String productName, double price) {
-        //shoppingCart.updateCart(id, productName, price);
-    }
-
-    public void displayCart() {
-        EtrackView cartView = new EtrackView();
-    }
-
-    //
-    public String promptForName() {
-
-        String userName = null;
-
-        boolean validName = false;
-        while(!validName) {
-            System.out.println("Please enter name: ");
-            String customerName = scanner.nextLine();
-            if (customerName.length() > 1 && (!customerName.matches("\\d{1,2}"))) {
-                userName = customerName;
-            }
-        }
-        return  userName;
-    }
-
 
     private void welcome() {
         System.out.println("\n");
         System.out.println("E-TRACK");
         System.out.println("\n");
     }
+
+     // prompt for name
+     public String promptForCustomerName() {
+
+         String userName = null;
+
+         boolean validName = false;
+         while(!validName) {
+             System.out.println("Please enter name: ");
+             String customerName = scanner.nextLine();
+             if (customerName.length() > 1 && (!customerName.matches("\\d{1,2}"))) {
+                 userName = customerName;
+             }
+         }
+         return  userName;
+     }
+
+    // show products available
+    public List<Product> showProductList() {
+        List<Product> productList = new ArrayList<>();
+        // for each prodct from Product and add it in productList
+        productList.add();
+    }
+
+    // prompt for priductID
+    public int promptForProductId() {
+        int productId = 0;
+        boolean validId = false;
+        while (!validId) {
+            System.out.println("Please enter an id: ");
+            String inputId = scanner.nextLine();
+            int inputIdInteger = Integer.parseInt(inputId);
+            if (productId < 0 || productId > 20 ) {
+                productId = inputIdInteger;
+            }
+        }
+        return productId;
+    }
+
+    // add product to cart by calling shopper method
+    public void addToCart() {
+        // call shopper add method and parse in productId
+        // get the product assciated with the id
+        //addProductToCart(productId);
+    }
+
+    // ask if they want to checkout or continue
+    public void promptToContinueShopping() {
+        System.out.println("Do you want to checkout or continue. Type Y to continue and N to checkout");
+        String yesOrNo = scanner.nextLine();
+        if (yesOrNo.equals("Y")){
+            promptForProductId();
+        }
+        else {
+            // checkout
+        }
+    }
+
+
+    // displau cart
+    public void displayCart() {
+
+    }
+
+    public void checkOut() {
+        //
+    }
+
 }
